6573dad34f59667cd590637c6f5f311347986652
Codexgsn <gustavopower2007@gmail.com>
2025-08-19 23:18:06 -0300

    dashboard corrigido

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 import os
 from datetime import datetime, timedelta, time
